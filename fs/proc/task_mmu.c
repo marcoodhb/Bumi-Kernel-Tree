93645c725d0d05cc40a1a5879e7005d049e14a89
XeroMz69 <adamyd18@gmail.com>
2025-03-06 11:41:51 +0000

    KernelSU: Add SuSFS 1.5.5
    Signed-off-by: XeroMz 69 <adamyd18@gmail.com>

--- fs/proc/task_mmu.c
+++ fs/proc/task_mmu.c
@@ -20,6 +20,9 @@
 #include <linux/uaccess.h>
 #include <linux/mm_inline.h>
 #include <linux/pkeys.h>
+#ifdef CONFIG_KSU_SUSFS_SUS_KSTAT
+#include <linux/susfs_def.h>
+#endif
 
 #include <asm/elf.h>
 #include <asm/tlb.h>
